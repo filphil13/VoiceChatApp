ccaf198ef9d01f5ecaea4b36830bb1de707d3111
filphil13 <filphil13@gmail.com>
2024-07-15 23:14:51 -0400

    chore: Refactor ChatServers.svelte and ServerCard.svelte components
    Refactor the ChatServers.svelte component to use the ServerCard.svelte component for rendering server cards. This improves code organization and reusability.
    
    Co-authored-by: [Author Name]

---
+++ Frontend/voicechatapp/src/lib/ServerCard.svelte
@@ -0,0 +1,13 @@
+<script>
+  	export let serverName;
+	export let serverImage;
+</script>
+
+<div class="inline-block px-3">
+    <div
+      class="w-64 h-64 max-w-xs rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
+    >
+        <img src={serverImage} alt="server" class="w-full object-cover" />
+        <h1 class=" text-center">{serverName}</h1>
+    </div>
+  </div>
